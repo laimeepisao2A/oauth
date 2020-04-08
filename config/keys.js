242300fd0c07fbb678c3dee1f994c7eacbf29b4b
Laimee Ramos <lpisao_180000001577@uic.edu.ph>
2020-04-09 02:29:58 +0800

    part1 commit

---
+++ config/keys.js
@@ -0,0 +1,22 @@
+module.exports = {
+    google:{
+        clientID: "493582541026-uh95e87d186rs006f5djo55td4lu93vm.apps.googleusercontent.com",
+        clientSecret: "uYAzycWAO7hSV3W65OPgsXtk"
+    },
+    facebook:{
+        clientID: "506030986733220",
+        clientSecret:"4f42bb9c38e29869e6576498316cf4f2"
+    },
+
+    postgresdb:{
+        user: "vlakksjpcoyzcv", 
+        host: "ec2-174-129-33-30.compute-1.amazonaws.com",
+        database: "d6bk3q3k0g0l3g", 
+        password: "594a142a925d9d6a431ee8ef7b325e4e1fc8a6670ecfa096c64365ad960ab5b9", 
+        port:"5432",
+        ssl:true,
+    },
+    session:{
+        cookieKey: 'thisismyuniquecookiekey'
+    }
+};
