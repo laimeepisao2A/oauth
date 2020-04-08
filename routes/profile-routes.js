9fe8a5773b9f16c777d914d83fc37f3f450e923a
Laimee Ramos <lpisao_180000001577@uic.edu.ph>
2020-04-09 02:26:24 +0800

    initial commit part6-1

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,18 +1,17 @@
 const router = require('express').Router();
 
 const authCheck = (req, res, next)=>{
-    console.log("*************************");
+    console.log("*********** Performing AuthCheck **************");
     console.log(req.user);
     if(!req.user){
-        // User is not logged in
+        // If user is not logged in then
         res.redirect('/auth/login');
     }
     else{
-        // User is logged in
+        // If user is logged in then
         next();
     }
 }
-
 router.get('/', authCheck, (req,res)=>{
     const user = req.user;    
     res.send('you are logged in, this is your profile - ' + user._name);
